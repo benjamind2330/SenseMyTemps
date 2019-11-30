574e08d05e5dd493be198462de30853980aff4c6
Ben Dart <benjamin.d2330@gmail.com>
2019-11-30 22:21:18 +0100

    add firebase reporting

--- SenseMyTemps.ino
+++ SenseMyTemps.ino
@@ -1,11 +1,16 @@
 // Wi-fi
 #include <ESP8266WiFi.h>
-
+#include <FirebaseArduino.h>
+#include <ArduinoJson.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #include "constants.h"
 #include "temperature_sensor.h"
 
 Thermometer thermometer(constants::TEMP_SENSOR_PIN);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, constants::NTP_ADDRESS.c_str(), constants::NTP_OFFSET, constants::NTP_INTERVAL);
 
 void setup() {
   // Serial
@@ -28,11 +33,36 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
 
+  // Firebase
+  Firebase.begin(constants::FIREBASE_HOST, constants::FIREBASE_AUTH);
+
+  // NTP
+  timeClient.begin();
 }
 
 void loop() {
 
-  Serial.println(thermometer.getTemp());
-  delay(1000);
+  auto startTime = millis();
+
+  timeClient.update();
+
+  const int capacity = JSON_OBJECT_SIZE(3);
+  StaticJsonBuffer<capacity> jb;
+  JsonObject& obj = jb.createObject();
+  obj["time"] = timeClient.getEpochTime();
+  obj["temperature"] = thermometer.getTemp();
+    
+  Firebase.push(constants::ROOM_NAME, obj); 
+  if (Firebase.failed()) { 
+     Serial.print("setting /number failed:"); 
+     Serial.println(Firebase.error());   
+     return; 
+  }
+
+  auto runTime = millis() - startTime;
+  
+  if (runTime < constants::LOOP_RATE_MS) {
+    delay(constants::LOOP_RATE_MS - runTime);
+  }
   
 }
